0b00e3e58e3d93c9263c6105bbef83c69a9bc1d0
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-05 13:57:39 -0300

    Update 1-03-operadores-logicos.c

--- 1-03-operadores-logicos.c
+++ 1-03-operadores-logicos.c
@@ -3,14 +3,10 @@
 
 int main(){
     
-    // conectivo de ligação &&, significa "e"
-    //atribui para a variavel uma condição, se for verdadeiro mostrar 1, se false mostra 0
     int i = 40;
     int condicao = (i>20) && (i<100);
     printf("%i\n", condicao);
     
-    // conectivo de ligação ||, significa "ou"
-    // no minimo uma das sentenças precisa ser verdeira
     int x = 10;
     int cond = 0;
     
@@ -19,7 +15,6 @@ int main(){
     
     cond = (x==3) || (x<3);
     printf("%i\n", cond);
-    
     
     return 0;
 }
