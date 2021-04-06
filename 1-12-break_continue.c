637294b6c9140946c4d12c7a7213a6187bf95c65
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-06 15:34:01 -0300

    Update 1-12-break_continue.c

--- 1-12-break_continue.c
+++ 1-12-break_continue.c
@@ -3,20 +3,15 @@
 
 int main(){
 
-    //break interrompe todo o ciclo
-    //continue interrompe apenas aquela rodada
     for(int x = 0; x <= 10; x++){
-        
         if(x == 4){
             printf("continue nao mostra a variavel atual\n");
             continue;
         }
-        
         if(x == 7){
             printf("break encerra o ciclo\n");
             break;
         }
-        
         printf("%i\n", x);
     }
     
