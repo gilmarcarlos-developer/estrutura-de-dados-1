49ac0cada0a1aa4218c7f1bc6e5ceddc1405203e
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-07 14:00:15 -0300

    Update 1-ep_01.4.c

--- 1-ep_01.4.c
+++ 1-ep_01.4.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-
+    //entre com 2 números e mostre quantos numeros primos existem entre eles
     int num1 = 0, num2 = 0, aux = 0;
     int primo = 0, qt_primo = 0;
     
@@ -13,17 +13,14 @@ int main(){
     
     for(num1; num1 <= num2; num1++){
         for(int y = 2; y < num1; y++){
-            if(num1 % y == 0){
+            if(num1 % y == 0)
                 primo++;
-            }
         }
-        if(primo == 0){
+        if(primo == 0)
             qt_primo++;
-        }
         primo = 0;
     }
    
     printf("Existem %i numeros primos entre %i e %i\n", qt_primo, aux, num2);
-
     return 0;
 }
