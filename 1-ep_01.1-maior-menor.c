a93a9f3a7926fb96919a6f416ec970252a96f7db
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-07 13:51:19 -0300

    Update 1-ep_01.1-maior-menor.c

--- 1-ep_01.1-maior-menor.c
+++ 1-ep_01.1-maior-menor.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
 int main(){
-
-    int n, numero;
-    int soma = 0, maior = 0, menor = 0;
+    //mostrar soma, maior e menor de n numeros digitados
+    int x, n = 0, numero = 0;
+    int soma, maior, menor;
     
     printf("\tExemplo 2!\nDigite um numero: ");
     scanf("%d", &n);
     
-    menor = numero + 1;
-    
-    for(int x = 0; x < n; x++){
+    printf("Agora digite %i numeros:\n", n);
+    scanf("%d", &numero);
+    soma = menor = maior = numero;
+
+    for(int x = 1; x < n; x++){
         scanf("%d", &numero);
-        
         soma = soma + numero;
         
-        if(numero > maior){
+        if(numero > maior)
             maior = numero;
-        }
-        if(numero < menor){
+        if(numero < menor)
             menor = numero;
-        }
     }
-    
+
     printf("\nSoma = %d, Maior = %d, Menor = %d", soma, maior, menor);
-    
     printf("\nFim do programa!\n");
     return 0;
 }
+
