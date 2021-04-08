e0a076664fe6acbf71e9e41c78b57d92c381fd3e
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-08 14:10:46 -0300

    Update 1-slides_exercicio2.c
    decidi usar o "for" ao invés do "do while"

--- 1-slides_exercicio2.c
+++ 1-slides_exercicio2.c
@@ -1,42 +1,28 @@
 #include <stdio.h>
 
 int main(){
-
     // mostrando maior, menor e soma
-    
     int n = 0, numeros = 0;
     int maior = 0, menor = 0, soma = 0;
-    int aux = 1;
     
     printf("Digite um numero: ");
     scanf("%i", &n);
     
     printf("Agora digite %i numeros: ", n);
-    do{
+    scanf("%i", &numeros);
+    soma = maior = menor = numeros;
+
+    for(int x = 1; x < n; x++){
         scanf("%i", &numeros);
-        
-        //maior numero
-        if(numeros > maior){
+
+        if(numeros > maior)
             maior = numeros;
-            if(aux == 1){
-                menor = numeros;
-                aux = 2;
-            }
-        }
-        
-        //menor numero
-        if(numeros < menor){
+        if(numeros < menor)
             menor = numeros;
-        }
-        
-        //soma de todos
+
         soma = soma + numeros;
-        n--;
+    }
 
-    }while(n != 0);
-
-    printf("Maior numero digitado = % i \nMenor numero digitado = %i \nSoma = %i", maior, menor, soma);
-    
-    printf("\n");
+    printf("Maior numero digitado = % i \nMenor numero digitado = %i \nSoma = %i\n", maior, menor, soma);   
     return 0;
 }
