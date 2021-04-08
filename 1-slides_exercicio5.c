f0a5b58eaece7d00212a895a5c5d111c2d689594
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-08 14:29:32 -0300

    Update 1-slides_exercicio5.c
    decidi usar função pra ter uma variável a menos e separar os scanf só por estética mesmo

--- 1-slides_exercicio5.c
+++ 1-slides_exercicio5.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 
+int soma(int num1, int num2, int num3){
+    return num1 + num2 + num3;
+}
+int multiplicacao(int num1, int num2){
+    return num1 * num2;
+}
+
 int main(){
- 
     int op = 1;
-    int a = 0, b = 0, c = 0, resultado = 0;
+    int a, b, c;
     
-    //menu
     while(op != 0){
-    printf("\nDigite a opcao desejada: \n1- soma dois numeros \n2- soma tres numeros \n3- multiplicacao de dois numeros \n0- sair\n");
-    scanf("%i", &op);
+        a = b = c = 0;
+        printf("\nDigite a opcao desejada: \n1- soma dois numeros \n2- soma tres numeros \n3- multiplicacao de dois numeros \n0- sair\n");
+        scanf("%i", &op);
     
-        //opção 1
         if(op == 1){
-            printf("\nVoce escolheu soma de dois numeros, digite dois numeros: ");
-            scanf("%i %i", &a, &b);
+            printf("\nVoce escolheu soma de dois numeros, digite dois numeros: \n");
+            scanf("%i", &a);
+            scanf("%i", &b);
             
-            resultado = a + b;
-            printf("A soma de %i + %i = %i\n", a, b, resultado);
-        
-        //opção 2
+            printf("A soma de %i + %i = %i\n", a, b, soma(a, b, c));        
         }else if (op == 2){
-            printf("\nVoce escolheu soma de tres numeros, digite tres numeros: ");
-            scanf("%i %i %i", &a, &b, &c);
-            
-            resultado = a + b + c;
-            printf("A soma de %i + %i + %i = %i\n", a, b, c, resultado);    
-            
-        //opção 3
+            printf("\nVoce escolheu soma de tres numeros, digite tres numeros: \n");
+            scanf("%i", &a);
+            scanf("%i", &b);
+            scanf("%i", &c);
+
+            printf("A soma de %i + %i + %i = %i\n", a, b, c, soma(a, b, c));    
         }else if(op == 3){
-            printf("\nVoce escolheu multiplicacao de dois numeros, digite dois numeros: ");
-            scanf("%i %i", &a, &b);
+            printf("\nVoce escolheu multiplicacao de dois numeros, digite dois numeros: \n");
+            scanf("%i", &a);
+            scanf("%i", &b);
             
-            resultado = a * b;
-            printf("A multiplicacao de %i * %i = %i\n", a, b, resultado);
+            printf("A multiplicacao de %i * %i = %i\n", a, b, multiplicacao(a, b));
             
         }else if(op == 0){
             printf("\nVoce escolheu sair,\nFim do programa!\n");
@@ -41,7 +43,5 @@ int main(){
             printf("\nOpcao invalida!\n");
         }
     }
-    
-    return 0;
-    
+    return 0;   
 }
