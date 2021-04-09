08725a3245cf752f1c040f4dc032f7862ded2bf1
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-09 15:48:43 -0300

    Update 2_array-2-matriz.c

--- 2_array-2-matriz.c
+++ 2_array-2-matriz.c
@@ -2,13 +2,8 @@
 #include <stdlib.h>
 
 int main(){
-
-    //matriz é um array de duas dimensões
-    
-    //forma geral: <tipo> <nome> [<numero_linhas>][<numero_colunas]
-    
     int matriz[3][4];
-    
+   
     //leitura
     for(int x = 0; x < 3; x++){
         for(int y = 0; y < 4; y++){
