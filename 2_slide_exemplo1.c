576c2f1edc39d5ca977d313f2e75f0ddd2d0d6b9
Gilmar Carlos <67771820+gilmarcarlos-developer@users.noreply.github.com>
2021-04-12 20:34:53 -0300

    Update 2_slide_exemplo1.c

--- 2_slide_exemplo1.c
+++ 2_slide_exemplo1.c
@@ -7,17 +7,14 @@ int main(){
     
     for(int x = 0; x < 10; x++){
         scanf("%i", &idades[x]);
-        
         soma = soma + idades[x];
     }
     
     media = soma / 10;
     
     for(int x = 0; x < 10; x++){
-        
-        if(idades[x] > media){
+        if(idades[x] > media)
             qt_acima_media++;
-        }
     }
     
     printf("\nA media de idade eh %i\nExistem %i pessoa(s) acima da idade media", media, qt_acima_media);
